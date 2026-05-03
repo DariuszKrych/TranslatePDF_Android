8b74d96e552a79f5ebd9bccb96a79bb5b5bdcd65
Dariusz Krych <darek.krych@gmail.com>
2026-05-03 13:15:06 +0200

    Test Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.dariuszkrych.translatepdf"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "TestUpdate_Blue"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
